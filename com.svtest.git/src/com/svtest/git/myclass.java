144c1b100bc46e51ace9987fda1847a99b4d6ec4
svshane <stefan@varkuti.de>
2014-09-30 13:38:14 +0200

    Update myclass.java
    sfg

--- com.svtest.git/src/com/svtest/git/myclass.java
+++ com.svtest.git/src/com/svtest/git/myclass.java
@@ -4,7 +4,7 @@ public class myclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// one more comment asdf asdf
+		// one more comment asdf asdf afk
 	}
 
 }
